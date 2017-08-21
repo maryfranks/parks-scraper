fa932f0f8f1694e3dd166d73eec4a25713ab5cfc
maryfranks <maryfranks@gmail.com>
2017-08-21 17:45:58 -0400

    update to scrape ontario parks page for camground urls

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const fs = require('fs');
 const Nightmare = require('nightmare');
 const nm = Nightmare({show: true});
-const baseUrl = 'http://www.nhm.ac.uk';
+const baseUrl = 'http://www.ontarioparks.com';
 let acc = [];
 
 // step gets all the urls you need to go to
-nm.goto(`${baseUrl}/discover/dino-directory/name/name-az-all.html`)
+nm.goto(`${baseUrl}/parksguide`)
   .evaluate(function() {
     let results = [];
-    let dinoLinks = document.querySelectorAll('.dino-list a');
+    // parkLinks has some duplicates for sites like Algonquin that have more
+    // than one campsite link in the main list
+    let parkLinks = document.querySelectorAll('.col-xs-3 a');
 
-    for(let i=0; i < dinoLinks.length; i++) {
-      results.push(dinoLinks[i].getAttribute('href'));
+    for(let i=0; i < parkLinks.length; i++) {
+      results.push(parkLinks[i].getAttribute('href'));
+      // add that there are links on the pages for camping, activities and facilities
     }
 
-    return results;
-  }).then(crawl).catch(bail);
+    let uniqueParks = results.filter(function (value, index, self) {
+        return self.indexOf(value) === index;
+    });
+
+    return uniqueParks;
+    // return results;
+  }).then(function(uniqueParks){
+    console.log(uniqueParks);
+  });
+  // .then(crawl).catch(bail);
 
 // recursive function: another way to loop
 function crawl(urls) {
