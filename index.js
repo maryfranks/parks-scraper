624f84227c544da13cafde9705badaf56a407875
maryfranks <maryfranks@gmail.com>
2017-08-22 14:04:18 -0400

    separate methods to get two classes of URLs, one array with main page and one with supplementary pages

--- index.js
+++ index.js
@@ -4,38 +4,63 @@ const nm = Nightmare({show: true});
 const baseUrl = 'http://www.ontarioparks.com';
 let acc = [];
 
-// step gets all the urls you need to go to
+// step 1: gets main park page urls and stores them in array called uniqueParks
 nm.goto(`${baseUrl}/parksguide`)
   .evaluate(function() {
     let results = [];
-    // parkLinks has some duplicates for sites like Algonquin that have more
-    // than one campsite link in the main list
     let parkLinks = document.querySelectorAll('.col-xs-3 a');
 
     for(let i=0; i < parkLinks.length; i++) {
       results.push(parkLinks[i].getAttribute('href'));
-      // add that there are links on the pages for camping, activities and facilities
     }
 
+    // parkLinks has some duplicates for sites like Algonquin that have more
+    // than one campsite link in the main list
     let uniqueParks = results.filter(function (value, index, self) {
         return self.indexOf(value) === index;
-    });
-
-// each park has subpages for camping, activities, a facilities
-    uniqueParks.map(function (park) {
-      let campingURL = park + '/camping';
-      uniqueParks.push(campingURL);
-      let activitiesURL = park + '/activities';
-      uniqueParks.push(activitiesURL);
-      let facilitiesURL = park + '/facilities';
-      uniqueParks.push(facilitiesURL);
     });
 
     return uniqueParks;
 
   }).then(function(uniqueParks){
-    console.log(uniqueParks);
-  });
+      console.log(uniqueParks);
+      });
+  // .then(crawl).catch(bail);
+
+
+// step 2: set of subpages with the real info we want and stores in array parkFeaturesURLs
+
+nm.goto(`${baseUrl}/parksguide`)
+  .evaluate(function() {
+    let results = []
+    let parkFeaturesURLs = [];
+
+    let parkLinks = document.querySelectorAll('.col-xs-3 a');
+
+    for(let i=0; i < parkLinks.length; i++) {
+      results.push(parkLinks[i].getAttribute('href'));
+    }
+
+    // parkLinks has some duplicates for sites like Algonquin that have more
+    // than one campsite link in the main list
+    let uniqueParks = results.filter(function (value, index, self) {
+        return self.indexOf(value) === index;
+    });
+
+    uniqueParks.map(function (park) {
+      let campingURL = park + '/camping';
+      parkFeaturesURLs.push(campingURL);
+      let activitiesURL = park + '/activities';
+      parkFeaturesURLs.push(activitiesURL);
+      let facilitiesURL = park + '/facilities';
+      parkFeaturesURLs.push(facilitiesURL);
+    });
+
+    return parkFeaturesURLs;
+
+  }).then(function(parkFeaturesURLs){
+      console.log(parkFeaturesURLs);
+      });
   // .then(crawl).catch(bail);
 
 // recursive function: another way to loop
@@ -46,11 +71,11 @@ function crawl(urls) {
       .end()
       .then(function(result) {
         acc = result;
-        let dinos = { dinosaurs: acc };
+        let parks = { ontarioParks: acc };
         // add feature: if file does not exist then create it
         // when it tries to write it throws an error
-        fs.writeFileSync('results/dinos.json', JSON.stringify(dinos), 'utf8');
-        console.log(`Finished scraping ${acc.length} dinos to results/dinos.json`);
+        fs.writeFileSync('results/ontarioparks.json', JSON.stringify(parks), 'utf8');
+        console.log(`Finished scraping ${acc.length} dinos to results/ontarioparks.json`);
       })
       .catch(bail);
   } else {
@@ -66,19 +91,29 @@ function crawl(urls) {
 
 function scrape(acc) {
   let result = {};
-  let keys = document.querySelectorAll('.dino-facts dt');
-  let values = document.querySelectorAll('.dino-facts dd');
 
-  for (let i=0; i<keys.length; i++) {
-    let words = keys[i].innerText.trim().split(' ')
-    let key = words.map(function(w, i) {
-      return w.toLowerCase();
-    }).join('_');
+  // get the park name (probably from both types of pages)
+  result['name'] = document.querySelector().innerText;
 
-    result[key] = values[i].innerText.trim();
-  }
+  // get the park features as boolean values from the features URLs
+  let keys = document.querySelectorAll();
+  result[key] = true;
 
-  result['name'] = document.querySelector('#dino-intro h2').innerText;
+
+  // Devon's code for dino scraper
+  // let keys = document.querySelectorAll('.dino-facts dt');
+  // let values = document.querySelectorAll('.dino-facts dd');
+  //
+  // for (let i=0; i<keys.length; i++) {
+  //   let words = keys[i].innerText.trim().split(' ')
+  //   let key = words.map(function(w, i) {
+  //     return w.toLowerCase();
+  //   }).join('_');
+  //
+  //   result[key] = values[i].innerText.trim();
+  // }
+
+  // result['name'] = document.querySelector('#dino-intro h2').innerText;
 
   acc.push(result);
   return acc;
